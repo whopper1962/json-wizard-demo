c953410fdf3e9894937d919c546994e904b5e070
Masashi Kawakami <dlold1962@gmail.com>
2022-11-24 09:16:26 +0900

    add file input form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
     <h3>XLSX JSON Converter</h3>
+    <input
+      type="file"
+      accept=".xls,.xlsx,.json"
+    >
+    <button
+      @click="execConvert()"
+    >
+      Convert
+    </button>
   </div>
 </template>
 
 <script>
-
+import xlsxJsonConverter from '@/lib/xlsx-json-converter';
 export default {
+  methods: {
+    execConvert () {
+      xlsxJsonConverter();
+    }
+  }
 }
 </script>
