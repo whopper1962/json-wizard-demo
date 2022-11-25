1125801ee7ddc0058e192eda7e6daf5ba3d708a4
Masashi Kawakami <dlold1962@gmail.com>
2022-11-25 22:47:25 +0900

    add xlsx view commponent

---
+++ src/components/XlsxView.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="xlsx-view">
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {};
+  },
+  props: {},
+  created () {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+.xlsx-view {
+  background-color: rgb(215, 215, 215);
+  /* float: right; */
+  /* height: 80vh; */
+  /* width: 50%; */
+  flex-grow: 1;
+}
+</style>
