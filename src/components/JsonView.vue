0dde17527b48acecc487633483261740268e1981
Masashi Kawakami <dlold1962@gmail.com>
2022-11-25 22:46:41 +0900

    add json view component

---
+++ src/components/JsonView.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="json-view">
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {};
+  },
+  props: {},
+  created () {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+.json-view {
+  /* float: right; */
+  background-color: rgb(121, 121, 121);
+  /* height: 80vh; */
+  flex-grow: 1;
+  /* width: 50%; */
+}
+</style>
