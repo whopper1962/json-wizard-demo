91bb0caa72861ccf5360cd6b23d0a29e28d82a77
Masashi Kawakami <dlold1962@gmail.com>
2022-11-25 22:48:36 +0900

    fix global style

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,15 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #2c3e50; */
+}
+body {
+  padding: 0;
+  margin: 0;
 }
 
 nav {
@@ -18,7 +22,7 @@ nav {
 }
 
 nav a {
-  font-weight: bold;
+  /* font-weight: bold; */
   color: #2c3e50;
 }
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="home">
     <h3>XLSX JSON Converter</h3>
-    <input
-      type="file"
-      accept=".xls,.xlsx,.json"
-    >
-    <button
-      @click="execConvert()"
-    >
-      Convert
-    </button>
+    <div class="converter-file-form">
+      <input
+        type="file"
+        accept=".xls,.xlsx,.json"
+      >
+      <button
+        @click="execConvert()"
+      >
+        Convert
+      </button>
+    </div>
+    <div class="converter-style">
+      <div>
+        <JsonView/>
+      </div>
+      <div>
+        <XlsxView/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import xlsxJsonConverter from '@/lib/xlsx-json-converter';
+import JsonView from '@/components/JsonView';
+import XlsxView from '@/components/XlsxView';
+
 export default {
+  components: {
+    JsonView,
+    XlsxView
+  },
   methods: {
     execConvert () {
       xlsxJsonConverter();
@@ -23,3 +40,17 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.home {
+  /* height: 100vh; */
+}
+.converter-style {
+  display: flex;
+  /* margin-top: 20px;
+  width: 100%; */
+}
+.converter-file-form {
+  /* height: 10ch; */
+}
+</style>
