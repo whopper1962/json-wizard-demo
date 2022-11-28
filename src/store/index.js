9cc0543e592e3c8ba65160ea33b82f16a7776439
Masashi Kawakami <dlold1962@gmail.com>
2022-11-29 08:54:29 +0900

    add error flags / json to state

--- src/store/index.js
+++ src/store/index.js
@@ -5,21 +5,41 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    json: {}
+    json: {},
+    duplicationError: false,
+    invalidKeyError: false
   },
   getters: {
     getJson (state) {
       return state.json;
+    },
+    getDuplicationErrorStatus (state) {
+      return state.duplicationError;
+    },
+    getInvalidKeyErrorStatus (state) {
+      return state.invalidKeyError;
     }
   },
   mutations: {
     SET_JSON (state, json) {
       state.json = json;
+    },
+    SET_DUPLICATION_ERROR_STATUS (state, status) {
+      state.duplicationError = status;
+    },
+    SET_INVALID_KEY_ERROR_STATUS (state, status) {
+      state.invalidKeyError = status;
     }
   },
   actions: {
     setJson (context, json) {
       context.commit('SET_JSON', json);
+    },
+    setDuplicationErrorStatus (context, status) {
+      context.commit('SET_DUPLICATION_ERROR_STATUS', status);
+    },
+    setInvalidKeyErrorStatus (context, status) {
+      context.commit('SET_INVALID_KEY_ERROR_STATUS', status);
     }
   },
   modules: {
