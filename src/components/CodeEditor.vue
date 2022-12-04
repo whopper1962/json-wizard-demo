ede8d051c2b3b6c099db79954fcd642e676b487b
Masashi Kawakami <dlold1962@gmail.com>
2022-12-05 03:54:38 +0900

    implement json editor component

---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,67 @@
+<template>
+  <PrismEditor
+    class="my-editor height-400"
+    v-model="inputedCode"
+    :highlight="highlighter"
+    line-numbers
+  >
+  </PrismEditor>
+</template>
+
+<script>
+import { PrismEditor } from 'vue-prism-editor';
+import 'vue-prism-editor/dist/prismeditor.min.css';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism-tomorrow.css';
+
+export default {
+  components: {
+    PrismEditor
+  },
+  data () {
+    return {
+      code: 'console.log("Hello World")'
+    };
+  },
+  props: {
+    value: [String, Number]
+  },
+  computed: {
+    inputedCode: {
+      get () {
+        return this.value;
+      },
+      set (code) {
+        this.$emit('input', code);
+      }
+    }
+  },
+  methods: {
+    highlighter(code) {
+      return highlight(code, languages.js);
+    }
+  }
+}
+</script>
+
+<style>
+.height-400 {
+  height: 85%;
+  margin: 0px 10px;
+}
+.my-editor {
+  width: inherit;
+  border-radius: 8px;
+  background: #2d2d2d;
+  color: #ccc;
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px;
+}
+.prism-editor__textarea:focus {
+  outline: none;
+}
+</style>
