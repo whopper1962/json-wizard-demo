c6d0da6f01131c995ef5256961e12fc28ed14b0e
Masashi Kawakami <dlold1962@gmail.com>
2022-12-18 22:44:33 +0900

    fix user manual page

--- src/views/UserManualView.vue
+++ src/views/UserManualView.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="user-manual">
-    <h3>{{ $t('menu.userManual') }}</h3>
+    <!-- <h3>{{ $t('menu.userManual') }}</h3> -->
     <div class="user-manual-contents">
-      {{ compiledMarkdownText }}
+      <span v-html="compiledMarkdownText"/>
     </div>
   </div>
 </template>
 <script>
-import manual from '@/assets/user-manual.md';
+// import manual from '@/assets/user-manual.md';
 import { marked } from 'marked';
 
 export default {
   data () {
     return {
+      markdownText: "## Manual"
     };
   },
   computed: {
     compiledMarkdownText () {
-      return marked(manual);
+      return marked(this.markdownText);
     }
   }
 }
@@ -28,7 +29,11 @@ export default {
   margin-top: 50px;
 }
 .user-manual-contents {
+  text-align: left;
   margin: auto;
+  border: solid black;
   height: 100%;
+  margin: 5px;
+  padding: 10px;
 }
 </style>
